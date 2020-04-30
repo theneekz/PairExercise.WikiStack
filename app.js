533c2d7c4ad43bf00da57fa7a75ea6107936176d
Niko Sward <nicholascsward@gmail.com>
2020-04-30 12:08:06 -0500

    connected postgres

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const { main } = require('./views');
+const { db } = require('./models');
 
 app.use(express.static(__dirname + '/public'));
 
@@ -13,6 +14,10 @@ app.get('/', (req, res) => {
 });
 
 const PORT = 3000;
+
+db.authenticate().then(() => {
+  console.log('Connected to the database');
+});
 
 app.listen(PORT, () => {
   console.log(`App listening in port ${PORT}`);
